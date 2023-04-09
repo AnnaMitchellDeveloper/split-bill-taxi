20c6edf2a84cadcdca7eecd93fd0705542539716
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-04-09 14:55:59 -0400

    Finished script to calculate amount the money for trip

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const calculateButton = document.querySelector("#calculateBtn");
 calculateButton.addEventListener("click", calculateAmount);
+const tipButton = document.querySelector("#addTip");
+tipButton.addEventListener("click", showTip);
+
+function showTip(event) {
+	event.preventDefault();
+	tip.style.display = "block";
+}
 
 function calculateAmount(event) {
 	event.preventDefault();
@@ -7,5 +14,23 @@ function calculateAmount(event) {
 	const numberOfPeopleInput = document.querySelector("#numberOfPeopleInput").value;
 	const tipInput = document.querySelector("#tip").value;
 
-	
+	if (billInput === "" || numberOfPeopleInput === "" || billInput < 1 || numberOfPeopleInput < 1){
+		Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Please, enter information about your trip!",
+    });
+	}
+
+	let amountPerPerson = billInput/numberOfPeopleInput;
+	let tipPerPerson = (billInput * tipInput)/numberOfPeopleInput;
+	let totalSum = amountPerPerson + tipPerPerson;
+
+	amountPerPerson = amountPerPerson.toFixed(2);
+	tipPerPerson = tipPerPerson.toFixed(2);
+	totalSum = totalSum.toFixed(2);
+
+	document.querySelector("#dividedBill").textContent = amountPerPerson;
+	document.querySelector("#dividedTip").textContent = tipPerPerson;
+	document.querySelector("#billAndTip").textContent = totalSum;
 }
